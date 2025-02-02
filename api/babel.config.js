02e979c6265afb3e488bc5f2b769968a0ec0af4f
l-furquim <lucas.furquim@sptech.school>
2025-02-02 13:33:16 -0300

    i

--- api/babel.config.js
+++ api/babel.config.js
@@ -1,12 +1,12 @@
-export default {
+module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-typescript'],
   plugins: [
     [
       'module-resolver',
       {
-        root: ['./dist'],
+        root: ['./src'],
         alias: {
-          '@': './dist',
+          '@': './dist', // Define o alias '@' para a pasta 'src'
         },
       },
     ],
