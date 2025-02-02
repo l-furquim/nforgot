6ee8261ca2243c4f0e3816cb771a7389c48ca52a
l-furquim <lucas.furquim@sptech.school>
2025-02-02 17:20:44 -0300

    fix again

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,10 +1,10 @@
 import { app } from "@/app";
 
-app.listen({ port: 3333, host: "0.0.0.0" }).then((err) => {
-  if(err){
-    console.log(err);
-    process.exit(1);
-  }
-
-  console.log("Server running in 3333");
-});
+try{
+  app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
+    console.log("Server running in 3333");
+  });
+}catch(err){
+  console.log(err);
+  process.exit(1);
+}
