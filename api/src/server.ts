3f250c8a2094ade27ad3fae209662a7711677495
l-furquim <lucas.furquim@sptech.school>
2025-02-02 17:15:38 -0300

    add all ips host in api

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,5 +1,10 @@
 import { app } from "@/app";
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: 3333, host: "0.0.0.0" }).then((err) => {
+  if(err){
+    console.log(err);
+    process.exit(1);
+  }
+
   console.log("Server running in 3333");
 });
