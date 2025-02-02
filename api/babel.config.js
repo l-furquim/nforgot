b91b6dbd894e07dc0504a031bf4c30c3d096688c
l-furquim <lucas.furquim@sptech.school>
2025-02-02 13:30:45 -0300

    fix js prolbema

---
+++ api/babel.config.js
@@ -0,0 +1,14 @@
+export default {
+  presets: ['@babel/preset-env', '@babel/preset-typescript'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./dist'],
+        alias: {
+          '@': './dist',
+        },
+      },
+    ],
+  ],
+};
